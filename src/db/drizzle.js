fc5437a4e723ce1fafbdcf51e7a6e20d2e164ceb
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-06-16 17:03:22 -0500

    the database was configured for Neon

--- src/db/drizzle.js
+++ src/db/drizzle.js
@@ -1,4 +1,4 @@
-import { config } from 'dotenv'
+/* import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
 import * as schema from './schema'
@@ -8,4 +8,15 @@ config({ path: '.env.local' })
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 })
-export const db = drizzle(pool, { schema })
+export const db = drizzle(pool, { schema }) */
+
+// In production with neon
+import { drizzle } from 'drizzle-orm/neon-http'
+import * as schema from './schema'
+import { config } from 'dotenv'
+import { neon } from '@neondatabase/serverless'
+
+config({ path: '.env.local' })
+
+const sql = neon(process.env.DATABASE_URL)
+export const db = drizzle(sql, { schema })
