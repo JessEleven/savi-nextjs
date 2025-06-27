197b5edaac58b355114200dc96a3b03375f7ab0d
Jesús Mendoza <133887362+JessEleven@users.noreply.github.com>
2025-06-27 15:34:38 -0500

    bug fixes for deployment

--- src/app/dash/layout.jsx
+++ src/app/dash/layout.jsx
@@ -1,10 +1,15 @@
+import { Suspense } from 'react'
 import DashNav from './components-dash/dash-nav'
 
 export default function DashLayout ({ children }) {
   return (
     <div className='main-container'>
-      <DashNav />
-      {children}
+      <Suspense fallback={null}>
+        <DashNav />
+      </Suspense>
+      <Suspense fallback={null}>
+        {children}
+      </Suspense>
     </div>
   )
 }
